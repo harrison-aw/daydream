dcab6d739833f600670cdd6f6ca52c017f4d5fa4
tony <harrison.aw@gmail.com>
2019-07-21 22:24:47 -0400

    Updated Aggregator implementation and docstring.

--- dnd35e/core.py
+++ dnd35e/core.py
@@ -20,28 +20,6 @@
 #  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #  SOFTWARE.
 
-#  MIT License
-#
-#  Copyright (c) 2019 Anthony Harrison
-#
-#  Permission is hereby granted, free of charge, to any person obtaining a copy
-#  of this software and associated documentation files (the "Software"), to deal
-#  in the Software without restriction, including without limitation the rights
-#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#  copies of the Software, and to permit persons to whom the Software is
-#  furnished to do so, subject to the following conditions:
-#
-#  The above copyright notice and this permission notice shall be included in all
-#  copies or substantial portions of the Software.
-#
-#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#  SOFTWARE.
-
 """Low-level, core functionality for DayDream3.5"""
 
 __all__ = ['DayDreamError', 'Aggregator']
@@ -74,67 +52,97 @@ class Condition:
         return self.text
 
 
-class Aggregator:
-    """Aggregate values from all objects attached to an instance."""
+def _is_private(name: str) -> bool:
+    """Return true if the name is private.
 
-    def __init_subclass__(cls, ignore: Optional[AbstractSet[str]] = None) -> None:
+    :param name: name of an attribute
+    """
+    return name.startswith('_')
+
+
+def _is_public(name: str) -> bool:
+    """Return true if the name is not private, i.e. is public.
+
+    :param name: name of an attribute
+    """
+    return not name.startswith('_')
+
+
+class Aggregator:
+    """Aggregate values from all objects attached to an instance.
+
+    This overrides attribute look up and allows combining (through
+    addition) values defined both on a particular instance and on its
+    attributes. This is used to allow multiple different sources to
+    modify a value.
+
+    For example, a character's strength is partially inherent and is
+    possibly modified by their race and levels in particular classes.
+    This class defines the interface by which all of these can be made
+    aware of each other with minimal boilerplate. To manage this, a
+    character will be an aggregator and have its own `strength`
+    attribute (i.e. the inherent part of its strength) and will add to
+    this value any attribute that also defines a `strength` attribute.
+    In this way, a class and a race that both define `strength` will
+    be added to the strength present on the character yielding a total
+    for that ability score.
+    """
+
+    def __init_subclass__(cls,
+                          ignore: Optional[AbstractSet[str]] = None) -> None:
         """Setup attributes to access directly."""
         super().__init_subclass__()
 
         if ignore is None:
-            ignore = set()
+            cls._ignore = set()
+        else:
+            cls._ignore = set(ignore)
 
-        cls._ignore = ignore
-        cls._instance_names = [k for k, v in vars(cls).items()
-                               if isinstance(v, property)]
+        cls._instance_names = {k for k, v in vars(cls).items()
+                               if isinstance(v, property)}
 
     def __init__(self) -> None:
         """Initialize attribute name tracker."""
         super().__init__()
-        self._instance_names = copy(self._instance_names)
+        self._instance_names: Set[str] = copy(self._instance_names)
 
     def __setattr__(self, name: str, value: Any) -> None:
         """Track any attributes that are added to an instance."""
-        if not name.startswith('_') and name not in self._known_names:
-            self._instance_names.append(name)
+        if _is_public(name) and name not in self._known_names:
+            self._instance_names.add(name)
         super().__setattr__(name, value)
 
     def __getattribute__(self, name: str) -> Any:
         """Aggregate value from each attribute if allowed."""
-        if name.startswith('_') or name in super().__getattribute__('_ignore'):
+        if _is_private(name) or name in super().__getattribute__('_ignore'):
             result = super().__getattribute__(name)
         else:
             values = []
 
             try:
-                val = super().__getattribute__(name)
+                values.append(super().__getattribute__(name))
             except AttributeError:
                 pass
-            else:
-                values.append(val)
 
-            for other_name in super().__getattribute__('_instance_names'):
-                if other_name != name:
-                    attribute = super().__getattribute__(other_name)
-                    try:
-                        val = getattr(attribute, name)
-                    except AttributeError:
-                        pass
-                    else:
-                        values.append(val)
+            for name_other in super().__getattribute__('_instance_names'):
+                if name_other != name:
+                    attribute = super().__getattribute__(name_other)
+                    if hasattr(attribute, name):
+                        values.append(getattr(attribute, name))
 
             if values:
                 result = reduce(add, values)
             else:
-                raise AttributeError
+                raise AttributeError(f'The desired attribute {name} could not'
+                                     f' be found')
         return result
 
     def __delattr__(self, name: str) -> None:
         """Remove deleted attributes from tracker."""
         super().__delattr__(name)
-        i = super().__getattribute__('_instance_names').index(name)
-        del super().__getattribute__('_instance_names')[i]
+        self._instance_names.remove(name)
 
     @property
     def _known_names(self) -> Set[str]:
-        return set(self._ignore | set(self._instance_names))
+        """Return all known names."""
+        return self._ignore | self._instance_names
